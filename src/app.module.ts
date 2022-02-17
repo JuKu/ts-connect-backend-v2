e414f36d153605a6f300b70171eb9da0c82267eb
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-02-18 00:43:03 +0100

    Add new AdminModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,11 @@ import {AppController} from "./app.controller";
 import {UserModule} from "./user/user.module";
 import {InfoModule} from "./info/info.module";
 import {ConfigModule} from "@nestjs/config";
+import { AdminModule } from './admin/admin.module';
+import { ProfileModule } from './profile/profile.module';
+import { StatisticsModule } from './statistics/statistics.module';
+import { ChatModule } from './chat/chat.module';
+import { MessagesModule } from './messages/messages.module';
 import configuration from "../config/configuration";
 
 @Module({
@@ -12,7 +17,7 @@ import configuration from "../config/configuration";
     // see also: https://docs.nestjs.com/techniques/configuration
     load: [configuration],
     isGlobal: true,
-  }), UserModule, InfoModule],
+  }), UserModule, InfoModule, AdminModule, ProfileModule, StatisticsModule, ChatModule, MessagesModule],
   controllers: [AppController],
   providers: [],
 })
