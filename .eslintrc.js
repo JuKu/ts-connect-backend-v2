bbc5e3f7e5d76b7d122f3d37c56d4c1e8207871e
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-02-17 18:04:30 +0100

    Add circle ci configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,33 @@
 module.exports = {
+  // added
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    ecmaVersion: "latest",
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
+  /*plugins: [
+      '@typescript-eslint/eslint-plugin'
+  ],*/
+  "plugins": [
+    "@typescript-eslint",
+    "security",
+    "security-node"
+  ],
+  /*extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+  ],*/
+  "extends": [
+    "google",
+    "plugin:security/recommended",
+    "plugin:security-node/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   root: true,
   env: {
@@ -16,6 +36,8 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+"use strict";
 
+/**
+ * this is the main file for the web-api application.
+ *
+ * @author Justin Kuenzel
+ *
+ * @copyright 2022, Justin Kuenzel, JuKuSoft
+ */
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+
+// get the server host and port
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || 3000;
+
+/**
+* The bootstrap function which starts the web server.
+*/
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
