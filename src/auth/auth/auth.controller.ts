5ed0c76cb409caebbaa2f2564cbf40976e4f3bb9
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-02-18 01:37:42 +0100

    Add redis cache

---
+++ src/auth/auth/auth.controller.ts
@@ -0,0 +1,9 @@
+import {Controller} from "@nestjs/common";
+
+@Controller("auth")
+/**
+ * The AuthController is responsible for authentication.
+ *
+ * @author Justin Kuenzel
+ */
+export class AuthController {}
