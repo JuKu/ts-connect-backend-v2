4a17c86211c91e580bc1ae92b96f8483d55f6cd9
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-02-24 23:14:51 +0100

    Fix some unit tests

--- src/user/user/user.service.spec.ts
+++ src/user/user/user.service.spec.ts
@@ -6,8 +6,47 @@ import {MongoMemoryServer} from "mongodb-memory-server";
 import {MongoDbTestService}
   from "../../database/mongo-dbtest/mongo-db-test.service";
 import {DatabaseModule} from "../../database/database.module";
-import {Connection} from "mongoose";
+import {Connection, Model} from "mongoose";
 import {DatabaseService} from "../../database/database/database.service";
-import {ConfigModule, ConfigService} from "@nestjs/config";
-import configuration from "../../../config/configuration";
+import {ConfigService} from "@nestjs/config";
 
+describe("UserService", () => {
+  let service: UserService;
+  let dbConnection: Connection;
+  let mongod: MongoMemoryServer;
+
+  beforeEach(async () => {
+    if (mongod !== undefined) {
+      console.error("mongod instance already exists");
+    }
+
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigService, /* DatabaseModule,*/
+        // rootMongooseTestModule(),
+        // DatabaseModule,
+        // MongooseModule.forFeature([{name: User.name, schema: UserSchema}]),
+      ],
+      providers: [UserService,
+        // DatabaseService,
+        {
+          provide: getModelToken(User.name),
+          useValue: Model,
+        }],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+    /* mongod = await module.get<MongoDbTestService>(MongoDbTestService)
+        .getInstance();
+
+    dbConnection = module
+        .get<DatabaseService>(DatabaseService).getDbHandle();
+
+    // start transaction
+    await dbConnection.startSession();*/
+  });
+
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+});
